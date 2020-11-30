a0fa3c01ca5c239545ed97fc5006585ba2c9a6f5
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 12:49:40 -0500

    ADD: SUB MENU

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Sidebar />
-    </>
+    </Router>
   );
 }
 
