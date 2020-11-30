2389a68f04081d8e58a8186e36d3f838311f36db
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 11:15:30 -0500

    ADD: Styled Components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+const Nav = styled.div`
+  background: #15171c;
+  height: 80px;
+`;
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <>
+      <Nav>Nav</Nav>
+    </>
   );
 };
 
