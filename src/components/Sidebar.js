737a44f1d3d85179e0a4ef0ed69c36320a6f75f8
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 11:05:38 -0500

    ADD: Icon

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/components/SubMenu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SubMenu = () => {
-  return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
-  );
-};
