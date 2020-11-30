e9d01afb32582c94a2ed0a74451a789408f0b447
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 13:31:21 -0500

    ADD: Google Fonts and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Overview from './components/pages/Overview';
 
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Switch>
+        <Route path="/overview" exact component={Overview} />
+      </Switch>
     </Router>
   );
 }
