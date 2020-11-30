27d33cf3b58f377590c2acfe042c5a7f05619e3b
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-30 13:40:52 -0500

    DONE: APP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Overview from './components/pages/Overview';
+import {
+  Reports,
+  ReportsOne,
+  ReportsThree,
+  ReportsTwo,
+} from './components/pages/Reports';
 
 function App() {
   return (
@@ -9,6 +15,10 @@ function App() {
       <Sidebar />
       <Switch>
         <Route path="/overview" exact component={Overview} />
+        <Route path="/reports" exact component={Reports} />
+        <Route path="/reports/reports1" exact component={ReportsOne} />
+        <Route path="/reports/reports2" exact component={ReportsTwo} />
+        <Route path="/reports/reports3" exact component={ReportsThree} />
       </Switch>
     </Router>
   );
